5312f5480b05562cc77f951957221dec92a6acc1
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-07 10:09:47 +0530

    Hey

---
+++ OracleJavaProAssign1/src/Test2.java
@@ -0,0 +1,20 @@
+
+public class Test2 {
+	public void Cricket(){
+		System.out.println("Cricket");
+	}
+	public void Motor(){
+		System.out.println("f1");
+	}
+	public void Foot(){
+		System.out.println("Foot Ball");
+	}  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test2 t1=new Test2();
+		t1.Cricket();
+		t1.Motor();
+		t1.Foot();
+	}
+
+}
